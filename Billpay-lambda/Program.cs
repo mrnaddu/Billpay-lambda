a6ab9a1b4e698733d98b012d84e266031fdddf26
Mohammed Nadeem <mohammadnadm5@gmail.com>
2024-07-17 20:59:07 +0530

    updated the latest

--- Billpay-lambda/Program.cs
+++ Billpay-lambda/Program.cs
@@ -1,110 +1,15 @@
-using Billpay_lambda.Interfaces;
-using Billpay_lambda.Managers;
-using Billpay_lambda.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-
-var builder = WebApplication.CreateBuilder(args);
-var Configuration = builder.Configuration;
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
-builder.Services.AddCognitoIdentity();
-
-// Add service dependencies
-builder.Services.AddTransient<IBillPayservice, BillPayservice>();
-builder.Services.AddTransient<BillPayManager>();
-builder.Services.AddTransient<ITokenService, TokenService>();
-
-// Configure Swagger generator
-builder.Services.AddSwaggerGen(c =>
+namespace Billpay_lambda;
+public class Program
 {
-    c.SwaggerDoc("v1",
-        new OpenApiInfo
-        {
-            Title = "Billpay Web API",
-            Version = "v1"
-        });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public static void Main(string[] args)
     {
-        In = ParameterLocation.Header,
-        Description = "Please enter into field the word 'Bearer' followed by a space and the JWT value",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
+        CreateHostBuilder(args).Build().Run();
+    }
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
             {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.Authority = builder.Configuration["Cognito:Authority"];
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidIssuer = builder.Configuration["Cognito:Authority"],
-        LifetimeValidator = (before, expires, token, param) => expires > DateTime.UtcNow,
-        ValidateIssuerSigningKey = true,
-        ValidateAudience = false
-    };
-});
-
-// Configure the CORS policy.
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(Configuration["App:CorsOrigins"],
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                   .AllowAnyMethod()
-                   .AllowAnyHeader();
-        });
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Billpay Web API");
-    });
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-app.UseRouting();
-
-app.UseCors(Configuration["App:CorsOrigins"]);
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Billpay-lambda/Startup.cs
@@ -0,0 +1,145 @@
+﻿using System.Text.Json.Serialization;
+using Billpay_lambda.Interfaces;
+using Billpay_lambda.Managers;
+using Billpay_lambda.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Billpay_lambda;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+        services.AddCognitoIdentity();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.Authority = Configuration["Cognito:Authority"];
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = Configuration["Cognito:Authority"],
+                ValidateLifetime = true,
+                LifetimeValidator = (before, expires, token, param) => expires > DateTime.UtcNow,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "Billpay Web API",
+                    Version = "v1"
+                });
+
+            // Configure Swagger to use JWT Bearer authentication
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        services.AddControllers().AddJsonOptions(opt =>
+        {
+            opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
+        services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .WithOrigins(Configuration["App:CorsOrigins"])
+                    .SetIsOriginAllowedToAllowWildcardSubdomains()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
+
+        // Add service dependencies
+        services.AddScoped<IBillPayservice, BillPayservice>();
+        services.AddScoped<BillPayManager>();
+        services.AddScoped<ITokenService, TokenService>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseCors();
+
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Billpay Web API v1");
+                c.RoutePrefix = "swagger";
+                c.DisplayOperationId();
+                c.DisplayRequestDuration();
+                c.DocExpansion(DocExpansion.None);
+                c.EnableDeepLinking();
+                c.EnableFilter();
+                c.ShowExtensions();
+                c.EnableValidator();
+                c.SupportedSubmitMethods(SubmitMethod.Get, SubmitMethod.Post, SubmitMethod.Put, SubmitMethod.Delete);
+                c.OAuthClientId("ClientId");
+                c.OAuthClientSecret("ClientSecret");
+                c.OAuthRealm("realm");
+                c.OAuthAppName("App Name");
+                c.OAuthScopeSeparator(" ");
+            });
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStatusCodePages();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
